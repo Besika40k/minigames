2a549659a4a221ac89700e517e95fe5a762967cd
Beso <besomeskhia40k@gmail.com>
2026-09-20 00:44:36 +0400

    feat: render the header in the app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,3 +1,4 @@
+import { createHeader } from '../components/header/header.ts';
 import { renderHomePage } from '../pages/home/home-page.ts';
 import { renderNotFoundPage } from '../pages/not-found/not-found-page.ts';
 import { Route, type RouteDefinition } from '../types/route.ts';
@@ -5,8 +6,9 @@ import { createElement } from '../utils/create-element.ts';
 import { Router } from './router.ts';
 
 export function startApp(): void {
+  const header: HTMLElement = createHeader();
   const main: HTMLElement = createElement('main');
-  document.body.append(main);
+  document.body.append(header, main);
 
   const routes: readonly RouteDefinition[] = [{ path: Route.Home, render: renderHomePage }];
   const router: Router = new Router(routes, main, renderNotFoundPage);
