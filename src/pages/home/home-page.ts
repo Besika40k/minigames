a71e9ab585b0e76f62a95f502daec68c5fc2785f
Beso <besomeskhia40k@gmail.com>
2026-09-20 16:44:46 +0400

    feat: show the leaderboard on the home page

--- src/pages/home/home-page.ts
+++ src/pages/home/home-page.ts
@@ -1,6 +1,7 @@
 import { createGameDevelopers } from './game-developers/game-developers.ts';
 import { createHero } from './hero/hero.ts';
+import { createLeaderboard } from './leaderboard/leaderboard.ts';
 
 export function renderHomePage(): readonly HTMLElement[] {
-  return [createHero(), createGameDevelopers()];
+  return [createHero(), createLeaderboard(), createGameDevelopers()];
 }
