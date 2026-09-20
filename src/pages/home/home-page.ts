0be04a6a6c027f83ddc3ffc5b64c0146f5127a6e
Beso <besomeskhia40k@gmail.com>
2026-09-20 15:50:42 +0400

    feat: show the game developers section on the home page

--- src/pages/home/home-page.ts
+++ src/pages/home/home-page.ts
@@ -1,5 +1,6 @@
+import { createGameDevelopers } from './game-developers/game-developers.ts';
 import { createHero } from './hero/hero.ts';
 
 export function renderHomePage(): readonly HTMLElement[] {
-  return [createHero()];
+  return [createHero(), createGameDevelopers()];
 }
