cacee8e62625c0b28bd8788a2c61c23a388db105
Beso <besomeskhia40k@gmail.com>
2026-09-20 15:50:30 +0400

    feat: add game developers content

---
+++ src/data/game-developers.ts
@@ -0,0 +1,12 @@
+import type { GameDevelopersContent } from '../types/game-developers.ts';
+
+export const GAME_DEVELOPERS_CONTENT: GameDevelopersContent = {
+  title: 'Are You a Game Developer?',
+  descriptionLines: [
+    "Want to see your game on MiniGames? We're always looking for fun,",
+    'engaging mini games to add to our platform. Submit your game',
+    'and reach thousands of players!',
+  ],
+  buttonText: 'Submit Form',
+  contact: 'or contact us at developers@minigames.com',
+};
