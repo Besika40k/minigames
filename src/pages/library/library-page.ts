8dfac3ec3cd754d575a13c1ad082f9a4125c5a01
Beso <besomeskhia40k@gmail.com>
2026-09-25 18:28:17 +0400

    feat(library): show the title section on the library page

--- src/pages/library/library-page.ts
+++ src/pages/library/library-page.ts
@@ -1,13 +1,5 @@
-import { createElement } from '../../utils/create-element.ts';
-
-const TITLE_ID = 'library-title';
+import { createLibraryIntro } from './library-intro/library-intro.ts';
 
 export function renderLibraryPage(): readonly HTMLElement[] {
-  return [
-    createElement('section', {
-      className: 'library',
-      attributes: { 'aria-labelledby': TITLE_ID },
-      children: [createElement('h1', { text: 'Game Library', attributes: { id: TITLE_ID } })],
-    }),
-  ];
+  return [createLibraryIntro()];
 }
