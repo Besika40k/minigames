08ef785c4c0ac301706b43882831e3416abf6336
Beso <besomeskhia40k@gmail.com>
2026-09-25 19:07:10 +0400

    feat(library): show the pagination on the library page

--- src/pages/library/library-page.ts
+++ src/pages/library/library-page.ts
@@ -1,6 +1,7 @@
 import { createGameList } from './game-list/game-list.ts';
 import { createLibraryIntro } from './library-intro/library-intro.ts';
+import { createPagination } from './pagination/pagination.ts';
 
 export function renderLibraryPage(): readonly HTMLElement[] {
-  return [createLibraryIntro(), createGameList()];
+  return [createLibraryIntro(), createGameList(), createPagination()];
 }
