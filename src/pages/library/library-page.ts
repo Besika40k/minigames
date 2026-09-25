e964786b7ba896af3c04042bdeced67e4f9d1492
Beso <besomeskhia40k@gmail.com>
2026-09-25 18:47:28 +0400

    feat(library): show the game list on the library page

--- src/pages/library/library-page.ts
+++ src/pages/library/library-page.ts
@@ -1,5 +1,6 @@
+import { createGameList } from './game-list/game-list.ts';
 import { createLibraryIntro } from './library-intro/library-intro.ts';
 
 export function renderLibraryPage(): readonly HTMLElement[] {
-  return [createLibraryIntro()];
+  return [createLibraryIntro(), createGameList()];
 }
